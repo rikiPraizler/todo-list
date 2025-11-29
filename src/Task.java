7dc9f9a03d154991445c51a2e7116bd751e6982b
riki <prtova@gmail.com>
2025-11-29 19:43:07 +0200

    Add getters, setters and toString

--- src/Task.java
+++ src/Task.java
@@ -25,4 +25,44 @@ public class Task {
         this.status = status;
     }
 
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public EnumStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(EnumStatus status) {
+        this.status = status;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Task: " +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status;
+    }
 }
